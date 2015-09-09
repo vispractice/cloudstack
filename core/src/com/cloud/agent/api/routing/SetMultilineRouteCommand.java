0fb8ec76e498821a7b26daf05f645d6a82f235f5
lihai2 <lihai@vispractice.com>
2015-09-08 18:35:36 -0700

    update multilineRouteCommand hashMap key for the label

--- core/src/com/cloud/agent/api/routing/SetMultilineRouteCommand.java
+++ core/src/com/cloud/agent/api/routing/SetMultilineRouteCommand.java
@@ -5,19 +5,21 @@ import java.util.HashMap;
 import com.cloud.agent.api.to.NicTO;
 //Andrew ling add, add the multiline route command for the VR
 public class SetMultilineRouteCommand extends NetworkElementCommand{
-	//In the routeRules HashMap,gateway is key, net and netmask is value. The store format like : <10.204.120.1; 10.204.104.0-255.255.255.0,10.204.105.0-255.255.255.0>
-	HashMap<String, String> routeRules;
+	//In the routeRules HashMap,The store format <multiline label,<gateway,net-netmask>> 
+	//The store format like :<ctcc,<10.204.120.1; 10.204.104.0-255.255.255.0,10.204.105.0-255.255.255.0>>
+	//The default multiline store format like :<ctcc;<10.204.120.1; 0.0.0.0,10.204.104.0-255.255.255.0,10.204.105.0-255.255.255.0>>
+	HashMap<String, HashMap<String, String>> routeRules;
 
 	public SetMultilineRouteCommand(){
 		
 	}
-	public SetMultilineRouteCommand(HashMap<String, String> routeRules){
+	public SetMultilineRouteCommand(HashMap<String, HashMap<String, String>> routeRules){
 		this.routeRules = routeRules;
 	}
-	public HashMap<String, String> getRouteRules() {
+	public HashMap<String, HashMap<String, String>> getRouteRules() {
 		return routeRules;
 	}
-	public void setRouteRules(HashMap<String, String> routeRules) {
+	public void setRouteRules(HashMap<String, HashMap<String, String>> routeRules) {
 		this.routeRules = routeRules;
 	}
 }
