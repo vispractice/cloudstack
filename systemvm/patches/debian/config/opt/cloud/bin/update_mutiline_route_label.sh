17c5d6d6be852fb94a2e177ecfc2e4bfe538bcb7
linghouwen <linghouwen@vispractice.com>
2016-08-31 17:34:27 +0800

    fix the bug in the update static nat commands.

--- systemvm/patches/debian/config/opt/cloud/bin/update_mutiline_route_label.sh
+++ systemvm/patches/debian/config/opt/cloud/bin/update_mutiline_route_label.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #Andrew ling update the mutiline label route table 
 
-local vmInstanceIp=$1
-local routeTableName=$2
-local oldRouteTableName=$(sudo ip rule show |grep "from $vmInstanceIp lookup"|awk -F' ' '{print $5}')
+routeTableName=$1
+vmInstanceIp=$2
+oldRouteTableName=$(sudo ip rule show |grep "from $vmInstanceIp lookup"|awk -F' ' '{print $5}')
 if [ -n "$oldRouteTableName" ]
 then
       sudo ip rule del from $vmInstanceIp table $oldRouteTableName pref 4000
